c43db750683e8075d4aee88f61df88ac8774197b
Bosko Kragulj <boskokragulj16@gmail.com>
2020-07-22 18:24:16 +0200

    User enters number of clients

--- multi_client.py
+++ multi_client.py
@@ -32,6 +32,7 @@ class Client:
     def __init__(self, cname):
             #super(BrokerFirst, self).__init__(cname,**kwargs)
             self.q = Queue()
+            self.name = cname
             self.ClientSocket = socket.socket()
             
     def connect(self, portI):
@@ -43,13 +44,23 @@ class Client:
             #Input = input('Say Something: ')
             #ClientSocket.send(str.encode(Input))
             Response = self.ClientSocket.recv(1024)
-            print("Client {}".format(self))
+            print("{} gets message {}".format(self.name, Response.decode('utf-8')))
             check = Response.decode('utf-8')
-            print(Response.decode('utf-8'))
+            #print(Response.decode('utf-8'))
             if check == "end":
                 print("CLOSE SOCKET")
                 self.ClientSocket.close()
-        
+                
+numberOfClients = int(input("Enter total number of clients: "))
+
+objs = list()
+for i in range(numberOfClients):
+    objs.append(Client("Client" + str(i)))
+
+for i in range(numberOfClients):
+    objs[i].connect(0)
+    
+"""      
 C1 = Client("C1")
 C1.connect(0)
 
@@ -65,11 +76,30 @@ C4.connect(0)
 C5 = Client("C5")
 C5.connect(0)
 
+print(C1)
+print(C2)
+print(C3)
+print(C4)
+print(C5)
+"""
+threads = []
+for i in range(numberOfClients):
+    t = threading.Thread(target=objs[i].recv_messages, args=[])
+    threads.append(t)
+    
+"""
 t1 = threading.Thread(target=C1.recv_messages, args=[])
 t2 = threading.Thread(target=C2.recv_messages, args=[])
 t3 = threading.Thread(target=C3.recv_messages, args=[])
 t4 = threading.Thread(target=C4.recv_messages, args=[])
 t5 = threading.Thread(target=C5.recv_messages, args=[])
+"""
+
+for i in range(numberOfClients):
+    threads[i].start()
+    time.sleep(.1)
+    
+"""
 
 t1.start()
 time.sleep(.1)
@@ -80,6 +110,7 @@ time.sleep(.1)
 t4.start()
 time.sleep(.1)
 t5.start()
+"""
 
 
 """
