20eb62e646f32aff0586a3da4a12d732b2211170
Bosko Kragulj <boskokragulj16@gmail.com>
2020-08-04 22:39:31 +0200

    Message exchange between nodes optimized

--- RoundRobin.py
+++ RoundRobin.py
@@ -8,16 +8,19 @@ import threading
 from threading import Event, Lock
 import string
 from random import *
+import psutil
 
 ServerSocket = socket.socket()
 host = '127.0.0.1'
 port = 1233
 ThreadCount = 0
+totalMessagesSentCount = 0
 
 #clients = []
 #clients_lock = threading.Lock()
 
 flag = 1
+available = True
 endMessage = False
 emptyQueues = 0
 event = Event()
@@ -31,23 +34,51 @@ except socket.error as e:
 print('Waiting for a Connection..')
 ServerSocket.listen(2)
 
+def get_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+cpufreq = psutil.cpu_freq()
+print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+
+print("CPU Usage Per Core:")
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+    print(f"Core {i}: {percentage}%")
+print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
 class Publisher():
     def __init__(self, cname):
         #super(BrokerFirst, self).__init__(cname,**kwargs)
         self.name = cname
         self.cnt = 0
+        self.end = False
         
     def generate_string(self, RecvBroker):
         global endMessage
-        while self.cnt < 20:
-            min_char = 8
-            max_char = 12
+        min_char = 8
+        max_char = 12
+        while self.cnt < 95:
             allchar = string.ascii_letters + string.digits
             data = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
-            RecvBroker.q.put(data)
+            if RecvBroker.q.qsize() < RecvBroker.numberOfClients:
+                RecvBroker.q.put(data)
+            else:
+                RecvBroker.q2.put(data)
             self.cnt = self.cnt + 1
-            time.sleep(.1)
+            #time.sleep(.1)
         endMessage = True
+        self.end = True
         #print("VREME SLANJA JE {}".format(toc - tic))
         print("END MESSAGE SET TO TRUE")
 
@@ -56,6 +87,7 @@ class Broker():
     def __init__(self, cname):
         #super(BrokerFirst, self).__init__(cname,**kwargs)
         self.q = Queue()
+        self.q2 = Queue()
         self.numberOfClients = 0
         self.name = ""
         self.visited = False
@@ -64,37 +96,56 @@ class Broker():
         self.messageCount = 0
         self.clientList = []
         self.clientNames = []
-        #self.clients = set()
         
     def threaded_client(self):
         global emptyQueues
         global endMessage
+        global totalMessagesSentCount
         #connection.send(str.encode('Welcome to the Server\n'))
         print("Broker {} salje poruku".format(self))
         print("VELICINA REDA U {} JE {}".format(self, self.q.qsize()))
+        print("VELICINA REDA2 U {} JE {}".format(self, self.q2.qsize()))
+        #event.set()
         
-        print("Duzina liste {}".format(len(self.clientList)))
-        for c in self.clientList:
-            #data = input("Enter message for client: ")
-            mutex.acquire()
-            data = self.q.get()
-            mutex.release()
-            print("{} sending {}".format(self, data))
-            c.sendall(str.encode(data))
-            time.sleep(.1)
-            if self.q.empty() == True:
-                break
-            
         if self.q.empty() == True and endMessage == True:
             print("{} broker has empty queue".format(self))
             emptyQueues += 1
             return
         
+        
+        print("Duzina liste {}".format(len(self.clientList)))
+        for c in self.clientList:
+            #data = input("Enter message for client: ")
+            print("GARISA")
+            if self.q.empty() == True and self.q.empty() == True:
+                event.set()
+                return
+            mutex.acquire()
+            data = self.q.get()
+            mutex.release()
+            print("{} sending {}".format(self, data))
+            c.sendall(str.encode(data))
+            totalMessagesSentCount += 1
+            if totalMessagesSentCount == 100:
+                event.set()
+                return
+            time.sleep(.1)
+        event.set()
+        #event.set()
+        
     def start_message_exchange(self):
         #send message from current broker to its neighbour broker
+        print("POKRECEM START MESSAGE EXCHANGE ZA {}".format(self))
         t = threading.Thread(target=self.ReadFromQueueSendToQueue, args=[self, self.neighbours[0]])
         t.start()
         time.sleep(.1)
+    
+    """
+    def start_queue_exchange(self):
+        t = threading.Thread(target=self.read_from_broker_queue, args=[])
+        t.start()
+        time.sleep(.1)
+    """
         
     def wait_for_conn(self):
         #global clients
@@ -114,18 +165,47 @@ class Broker():
     #method for moving messages that could not be sent from 
     #current to next broker           
     def ReadFromQueueSendToQueue(self, src, dst):
+        global endMessage
         while True:
-            while self.q.empty() == False:
+            event.wait()
+            while src.q2.empty() == False:
+                #print("GARIIIIIIII")
                 #take message from queue and send it to next broker queue
-                a = src.q.get()
+                a = src.q2.get()
+                if dst.q.qsize() < dst.numberOfClients:
+                    dst.q.put(a)
+                    break
+                else:
+                    dst.q2.put(a)
+                event.clear()
                 #print("PREPISUJEM {} iz {} u {}".format(a, src, dst))
-                dst.q.put(a)
+                #dst.q2.put(a)
                 #send.wait()
+    
+    """
+    1. izracunati koliko krugova ce obici poruke u grafu
+    2. pomnoziti broj krugova sa brojem klijenata svakog cvora
+    3. ostaviti taj broj u q, a ostalo prebaciti u q2
+    4. poruke koje se nalaze u q2 kruze kroz graf
+    
+    
+    def read_from_broker_queue(self):
+        global endMessage
+        while True:
+            #event.wait()
+            while self.q.qsize() < self.numberOfClients and endMessage == False:
+                print("GARIIIIIII MOOOOJ")
+                data = self.q2.get()
+                self.q.put(data)
+            #event.clear()
+    """
+    
         
 #graph       
 class Graph:
     nodes = list()
     edges = list()
+    cnt = 0
 
     #constructor
     def __init__(self):
@@ -152,10 +232,12 @@ class Graph:
         if flag == 1:
             s.wait_for_conn()
             s.start_message_exchange()
+            #s.start_queue_exchange()
             print("Sacekao sam konekcije")
         elif flag == 2:
             #for c in s.clientList:
             s.threaded_client()
+            print("GARISONER {}".format(s))
         else:
             s.close_connection()
         #for c in clients:
@@ -184,6 +266,7 @@ class Graph:
                         print("Waiting for conn")
                         broker.wait_for_conn()
                         broker.start_message_exchange()
+                        #broker.start_queue_exchange()
                     elif flag == 2:
                         print()
                         print("Sending messages")
@@ -243,11 +326,31 @@ def Get_Predefined_Dag():
     print("NAPRAVIO SAM KLIJENTE")
     
     #testing queue
+    """
+    max_char = 12
+    min_char = 8
+    cnt = 0
+    ukupnoPoruka = 0
+    for i in range(nodes):
+        cnt = 0
+        while cnt < 10:
+            ukupnoPoruka += 1
+            allchar = string.ascii_letters + string.digits
+            data = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
+            objs[i].q.put(data)
+            cnt = cnt + 1
+            time.sleep(.1)
+        #objs[0].q.put("GArisa")
+    print("IMAM OVOLIKO CNT {}".format(ukupnoPoruka))
+    """
+                
+    
     objs[0].q.put("Message One")
     objs[0].q.put("Message Four")
     objs[0].q.put("Message Five")
     objs[0].q.put("Message Six")
     objs[0].q.put("Liman")
+    
     # Append adjacencies
     for i in range(len(adjacency)):
         G.edges.append(adjacency[i])
@@ -260,6 +363,10 @@ graph = Get_Predefined_Dag()
 graph.BFS(graph.nodes[0])
 print()
 print()
+P1 = Publisher("Publisher")
+t1 = threading.Thread(target=P1.generate_string, args=[graph.nodes[0]])
+tic = time.perf_counter()
+t1.start()
 print("Flag set")
 print("DUZINA GRAFA {}".format(len(graph.nodes)))
 flag = 2 
@@ -269,16 +376,13 @@ for c in graph.nodes[0].clientList:
     
 for c in graph.nodes[1].clientList:
     print("Node 1 {}".format(c))
-"""   
-P1 = Publisher("Publisher")
-    
-t1 = threading.Thread(target=P1.generate_string, args=[graph.nodes[0]])
-tic = time.perf_counter()
-t1.start()
+"""
+
+#time.sleep(10)
 
 while True:
     graph.BFS(graph.nodes[0])
-    if emptyQueues == len(graph.nodes):
+    if totalMessagesSentCount == 100:
         print("USAO SAM NA KRAJ")
         toc = time.perf_counter()
         break
@@ -288,7 +392,31 @@ CPU_Pct=str(round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)
 #print results
 print("CPU Usage = " + CPU_Pct)
 print("Elapsed time: {}".format(toc - tic))
+print()
+print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+print()
+print("CPU Usage Per Core:")
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+    print(f"Core {i}: {percentage}%")
+print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
+svmem = psutil.virtual_memory()
+print(f"Total: {get_size(svmem.total)}")
+print(f"Available: {get_size(svmem.available)}")
+print(f"Used: {get_size(svmem.used)}")
+print(f"Percentage: {svmem.percent}%")
+print("="*20, "SWAP", "="*20)
+# get the swap memory details (if exists)
+swap = psutil.swap_memory()
+print(f"Total: {get_size(swap.total)}")
+print(f"Free: {get_size(swap.free)}")
+print(f"Used: {get_size(swap.used)}")
+print(f"Percentage: {swap.percent}%")
+
 flag = 3
+graph.BFS(graph.nodes[0])
 #interate through the graph to close connections
 print("Closing connections")
 #graph.BFS(graph.nodes[0]) 
